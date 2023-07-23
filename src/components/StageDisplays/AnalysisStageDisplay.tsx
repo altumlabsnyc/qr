76194e80bad3ad3e8e1e7f4c9035530cf61f845b
Alex Godfrey <basilisk@altumlabs.co>
2023-07-23 11:39:28 -0400

    Add seo to necessary page

--- src/components/StageDisplays/AnalysisStageDisplay.tsx
+++ src/components/StageDisplays/AnalysisStageDisplay.tsx
@@ -1,4 +1,5 @@
 import { LabOrder, Metadata } from "@/types/DisplayTypes";
+import Head from "next/head";
 import Footer from "../Footer";
 import ProductInfo from "../ProductInfo";
 
@@ -9,10 +10,19 @@ interface Props {
 
 export default function AnalysisStageDisplay({ metadata, lab_order }: Props) {
   return (
-    <div className="prose max-w-none text-center leading-normal flex flex-col items-center">
-      <ProductInfo metadata={metadata} lab_order={lab_order} />
-      <p>Analysis stage display</p>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Plantalysis by Altum Labs - QR Site</title>
+        <meta
+          name="description"
+          content="Get lab results fast, easy, and transparently."
+        />
+      </Head>
+      <div className="prose max-w-none text-center leading-normal flex flex-col items-center">
+        <ProductInfo metadata={metadata} lab_order={lab_order} />
+        <p>Analysis stage display</p>
+        <Footer />
+      </div>
+    </>
   );
 }
