b7260cb58b30d9b6eb7d148222e949c4811ea57f
Alex Godfrey <basilisk@altumlabs.co>
2023-08-08 16:29:54 -0400

    Add link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,21 @@
 export default function Footer() {
   return (
     <div className="text-center">
-      <h1 className="mb-2 text-xl font-extrabold text-gray-900 dark:text-white md:text-5xl">
+      <h1 className="mb-2 text-xl font-extrabold text-gray-900 dark:text-white md:text-4xl">
         <p className="mb-1 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
           Need fast lab testing?
         </p>
-        <p className="mt-0 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+        <a
+          href="https://plantalysis.com"
+          className="mt-0 font-extrabold text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"
+        >
           Visit plantalysis.com
-        </p>
+        </a>
       </h1>
       <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
         <p className="my-1">(929) 276-2706</p>
         <p className="my-1">team@altumlabs.co</p>
       </div>
     </div>
-  );
+  )
 }
